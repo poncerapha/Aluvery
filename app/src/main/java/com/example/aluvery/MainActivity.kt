dbbe8e0e6ff3938503c8b88a77f4bc68deba7183
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-10-28 05:55:27 -0300

    feat: adding surface

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -24,14 +26,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
 import com.example.aluvery.ui.theme.Purple500
 import com.example.aluvery.ui.theme.Teal200
@@ -56,34 +63,58 @@ fun MyFirstComposable() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItemPreview() {
-    Column(
+    Surface(
         modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)
+            .padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(
-                    Brush.horizontalGradient(
-                        listOf(
-                            Purple500, Teal200
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        Brush.horizontalGradient(
+                            listOf(
+                                Purple500, Teal200
+                            )
                         )
                     )
-                )
 
-        )
-        Image(
-            modifier = Modifier
-                .offset(y = ((-50).dp))
-                .align(CenterHorizontally)
-                .size(100.dp)
-                .clip(shape = CircleShape),
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Product Image"
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize/2)
+                        .align(BottomCenter)
+                        .clip(shape = CircleShape),
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Product Image"
+                )
+            }
+            Spacer(modifier = Modifier.height(imageSize/2))
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700)
+                )
+                Text(
+                    modifier = Modifier.padding(top = 8.dp),
+                    text = "R$ 14,99",
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
     }
 }
