c1eb674bbdb43a1633c8df0cdc635ca1164def71
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-11-01 00:36:56 -0300

    feat: create image form

--- app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/activities/ProductFormActivity.kt
@@ -1,13 +1,17 @@
 package br.com.alura.aluvery.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -18,12 +22,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.alura.aluvery.R
+import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.ui.theme.AluveryTheme
+import coil.compose.AsyncImage
+import java.math.BigDecimal
 
 class ProductFormActivity : ComponentActivity() {
 
@@ -43,7 +53,9 @@ class ProductFormActivity : ComponentActivity() {
 fun ProductForm() {
     Column(
         modifier = Modifier
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         var url by remember { mutableStateOf("") }
@@ -58,6 +70,19 @@ fun ProductForm() {
             fontSize = 28.sp,
             fontWeight = FontWeight(400)
         )
+
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
 
         TextField(
             modifier = Modifier
@@ -108,7 +133,20 @@ fun ProductForm() {
         )
 
         Button(
-            onClick = { /*TODO*/ }) {
+            onClick = {
+                val convertedPrice = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+                val product = Product(
+                    name = name,
+                    image = url,
+                    price = convertedPrice,
+                    description = description
+                )
+                Log.i("ProductFormActivity", "ProductFormScreen: $product")
+            }) {
             Text(
                 text = "Salvar",
                 modifier = Modifier
