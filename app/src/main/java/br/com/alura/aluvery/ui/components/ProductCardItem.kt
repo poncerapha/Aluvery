3e2fcf638ee02e1b92d421a32affd1fa094d1b29
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-11-04 14:29:45 -0300

    feat: save UI states

--- app/src/main/java/br/com/alura/aluvery/ui/components/ProductCardItem.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/components/ProductCardItem.kt
@@ -14,7 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -37,7 +37,7 @@ fun ProductCardItem(
     elevation: Dp = 4.dp,
     modifier: Modifier = Modifier
 ) {
-    var expanded by remember { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
     Card(
         modifier
             .fillMaxWidth()
